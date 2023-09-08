a687528bee917afb064518758dd12903f3ad9dfb
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-09-08 20:05:13 +0300

    cansel fetch fubc

--- Add_Photos.py
+++ Add_Photos.py
@@ -5,26 +5,9 @@ import urllib.parse
 
 from pathlib import Path
 from urllib.parse import urlparse
+from fetch_spacex_images import fetch_spacex_last_launch
 
 
-def fetch_spacex_last_launch(find_url, i):
-    print("find_url", find_url)
-    filename = f'EPIC{i}.jpeg' #f'space_{value}.jpeg'
-    print(filename)
-    
-    #Создание папки, если ее не существует
-    try:
-        path = Path(f"images/{filename}")
-        path.parent.mkdir(parents=True, exist_ok=True)
-
-        response = requests.get(find_url) #, params=payload
-        print("response.url:", response.url)
-        print("response.status_code:", response.status_code)
-        print("-------")
-        with open(path, 'wb') as file:
-            file.write(response.content)
-    except:
-        print("error")
         
 
     # links=text["links"]["flickr"]["original"]
