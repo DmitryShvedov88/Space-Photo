194fbfa933b00d513b60a5d8670ca89d0a3a1825
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-09-29 21:55:45 +0300

    publish text to Telegram channel.

--- bot/main.py
+++ bot/main.py
@@ -7,8 +7,10 @@ load_dotenv(find_dotenv())
 TG_Token = os.getenv("TG_Token")
 chat_id = os.getenv("chat_id")
 bot = telegram.Bot(token=TG_Token)
-bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_message(chat_id=chat_id, text="Dima, i'll sand to you photo.")
+bot.send_document(chat_id=chat_id, document=open('C:/Users/nivka/Desktop/Обучение Питон/Мои проекты/images/space_0.jpeg', 'rb'))
 
-
+# updates = bot.get_updates()
+# print([u.message.photo for u in updates if u.message.photo])
 # if __name__ == "__main__":
 #     bot.polling(none_stop=True, interval=0)
