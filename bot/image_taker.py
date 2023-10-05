1249dab61b9c8b8321c0e843fb84f914b43c6d87
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-10-05 20:24:58 +0300

    fixed bag in imge taker

--- bot/image_taker.py
+++ bot/image_taker.py
@@ -1,23 +1,16 @@
 import os
-import argparse
 
 
 def takeFiles():
     directory = "C:/Users/nivka/Desktop/Обучение Питон/Мои проекты/images"
-    global file, name
+    global name
     images=[]
     try:
         filesindir = os.listdir(directory)
-        #print(filesindir)
         for filesindirs in filesindir:
-            #print(filesindirs)
             name = os.path.join(filesindirs)
             path = os.path.join(str(directory), name)
-            images.append(path)        
-        #     func(True)
-        # nm.lineEdit.clear()
-        # ui.lineEdit.clear()
-        # ui.lineEdit_2.clear()
+            images.append(path)
         return images
     except Exception as exp:
         print(str(exp), "err")
