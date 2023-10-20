ef0ab97af3dfa14dd56fb5fc8cc585e819856e6a
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-10-20 21:10:19 +0300

    clarified the name of the variable

--- download_image.py
+++ download_image.py
@@ -2,8 +2,8 @@ import requests
 from pathlib import Path
 
 
-def download_image(typer, photo_format, find_url, i):
-    filename = f'{typer}_{i}.{photo_format}'
+def download_image(name, photo_format, find_url, i):
+    filename = f'{name}_{i}.{photo_format}'
     try:
         path = Path(f"images/{filename}")
         path.parent.mkdir(parents=True, exist_ok=True)
