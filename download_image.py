d8d7184569e71c895cc02d57b3426752d6c1fa2b
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-10-20 21:18:20 +0300

    corrected the name of the variable

--- download_image.py
+++ download_image.py
@@ -2,12 +2,12 @@ import requests
 from pathlib import Path
 
 
-def download_image(name, photo_format, find_url, i):
+def download_image(name, photo_format, down_load_link, i):
     filename = f'{name}_{i}.{photo_format}'
     try:
         path = Path(f"images/{filename}")
         path.parent.mkdir(parents=True, exist_ok=True)
-        response = requests.get(find_url)
+        response = requests.get(down_load_link)
         print("response.status_code", response.status_code)
         with open(path, 'wb') as file:
             file.write(response.content)
