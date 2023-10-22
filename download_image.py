a7243137ad20d333ab1e4239ec011155b635f0eb
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2023-10-22 10:23:13 +0300

    response.raise_for_status() before download

--- download_image.py
+++ download_image.py
@@ -8,6 +8,7 @@ def download_image(name, photo_format, down_load_link, i):
         path = Path(f"images/{filename}")
         path.parent.mkdir(parents=True, exist_ok=True)
         response = requests.get(down_load_link)
+        response.raise_for_status()
         with open(path, 'wb') as file:
             file.write(response.content)
     except requests.HTTPError:
